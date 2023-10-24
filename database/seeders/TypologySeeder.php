6b01063060bfd4e920d5b1b8589be98140c86680
Simone Marino <simo.marino@outlook.it>
2023-10-24 20:57:37 +0200

    Add icons migration to Typology table

--- database/seeders/TypologySeeder.php
+++ database/seeders/TypologySeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Typology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class TypologySeeder extends Seeder
 {
@@ -18,24 +19,51 @@ class TypologySeeder extends Seeder
         $typologies = [
             [
                 'name' => 'Italian',
+                'icon' => 'fi fi-it'
             ],
             [
                 'name' => 'Mexican',
+                'icon' => 'fi fi-mx'
             ],
             [
                 'name' => 'Japanese',
+                'icon' => 'fi fi-jp'
             ],
             [
                 'name' => 'French',
+                'icon' => 'fi fi-fr'
             ],
             [
                 'name' => 'Chinese',
+                'icon' => 'fi fi-cn'
             ],
+            [
+                'name' => 'Argentinian',
+                'icon' => 'fi fi-ar'
+            ],
+            [
+                'name' => 'Australian',
+                'icon' => 'fi fi-au'
+            ],
+            [
+                'name' => 'Brazilian',
+                'icon' => 'fi fi-br'
+            ],
+            [
+                'name' => 'Cambodian',
+                'icon' => 'fi fi-kh'
+            ],
+            [
+                'name' => 'Thai',
+                'icon' => 'fi fi-th'
+            ],
+
         ];
 
         foreach ($typologies as $typology) {
             Typology::create([
                 "name" => $typology["name"],
+                "icon" => $typology["icon"],
             ]);
         };
     }
